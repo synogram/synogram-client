db65a75eea785a1e038ec1ed68e086795159fab8
jhwan7 <46652246+jhwan7@users.noreply.github.com>
2020-11-10 10:04:07 -0500

    Create SearchBar Component

--- src/components/SearchBar/searchbar.js
+++ src/components/SearchBar/searchbar.js
@@ -1,8 +1,42 @@
 import React from "react";
 import styles from "./SearchBar.module.scss";
+import searchIcon from "../../assets/icons/searchIcon.svg";
+import messages from "../../constants/Messages";
 
 const SearchBar = () => {
-  return <div></div>;
+  const handleOnEnter = (event) => {
+    event.preventDefault();
+
+    // TODO: IMPLEMENT ON ENTER (SEARCH)
+  };
+
+  const handleOnFocus = (event) => {
+    event.target.placeholder = "";
+  };
+
+  const handleOnBlur = (event) => {
+    event.target.placeholder = messages.searchPlaceHolder;
+  };
+
+  return (
+    <div className={styles.searchbarContainer}>
+      <form
+        className={styles.searchBar}
+        onSubmit={(event) => handleOnEnter(event)}
+      >
+        <input
+          type="text"
+          spellCheck="false"
+          placeholder={messages.searchPlaceHolder}
+          onFocus={(event) => handleOnFocus(event)}
+          onBlur={(event) => handleOnBlur(event)}
+        />
+      </form>
+      <button className={styles.searchButton}>
+        <img src={searchIcon} className={styles.searchIcon} />
+      </button>
+    </div>
+  );
 };
 
 export default SearchBar;
--- src/containers/pages/HomePage/HomePage.js
+++ src/containers/pages/HomePage/HomePage.js
@@ -1,9 +1,14 @@
 import React from "react";
 import styles from "./HomePage.module.scss";
+import SearchBar from "../../../components/SearchBar/SearchBar";
 
 class HomePage extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
   }
 }
 
