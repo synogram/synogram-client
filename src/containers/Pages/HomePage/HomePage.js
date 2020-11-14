649c2f46d8e033640e3c8fb5a373c23416ce8799
Ewhan Han <40779221+EwhanHan@users.noreply.github.com>
2020-11-14 18:52:35 -0500

    Feature/create background image (#7)
    * Created title, icon, and description with basic styling
    
    * remove unintentional scss change
    
    Co-authored-by: jhwan7 <46652246+jhwan7@users.noreply.github.com>

--- src/containers/Pages/HomePage/HomePage.js
+++ src/containers/Pages/HomePage/HomePage.js
@@ -1,15 +1,26 @@
 import React from "react";
 import styles from "./HomePage.module.scss";
 import SearchBar from "../../../components/SearchBar/SearchBar";
+import messages from "../../../constants/Messages";
+import searchIcon from "../../../assets/icons/searchIcon.svg";
 
 class HomePage extends React.Component {
-  render() {
-    return (
-      <div>
-        <SearchBar />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className={styles.homepage}>
+                <div className={styles.magnify_glass_container}>
+                    <div className={styles.magnify_glass}>
+                        <div className={styles.inner_glass}/>
+                        <div className={styles.glass_handle}/>
+                    </div>
+                </div>
+                <h1 className={styles.homepage_title}>SYN<img src={searchIcon}
+                                                              className={styles.searchIconHomepage} alt={"synogram icon"}></img>GRAM</h1>
+                <SearchBar/>
+                <div className={styles.homepage_description}><p>{messages.front_page_description}</p></div>
+            </div>
+        );
+    }
 }
 
 export default HomePage;
