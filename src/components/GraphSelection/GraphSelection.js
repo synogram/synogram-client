bd1a9d70d9c3d90d81f852a62e769af9db6d1bbe
jhwan7 <46652246+jhwan7@users.noreply.github.com>
2021-01-19 02:27:20 -0500

    add  redux to description panel

--- src/components/GraphSelection/GraphSelection.js
+++ src/components/GraphSelection/GraphSelection.js
@@ -1,15 +1,16 @@
-import { render } from 'enzyme';
-import React from 'react';
-import styles from "./GraphSelection.module.scss"
-import { useSelector } from 'react-redux'
+import {render} from "enzyme";
+import React from "react";
+import styles from "./GraphSelection.module.scss";
+import {useSelector} from "react-redux";
 const GraphSelection = () => {
-    const definition = useSelector(state => state.dictionary.definition)
-    return (
-        <div className = {styles.container}>
-            <p className = {styles.description}>{definition}</p>
-            <button>Tree Graph</button>
-        </div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <p className={styles.description}>
+        Observe the related words in a tree graph format
+      </p>
+      <button>Tree Graph</button>
+    </div>
+  );
+};
 
 export default GraphSelection;
--- src/containers/Descriptions/DescriptionWordInfo/DescriptionWordInfo.js
+++ src/containers/Descriptions/DescriptionWordInfo/DescriptionWordInfo.js
@@ -1,23 +1,25 @@
 import React from "react";
 import styles from "./DescriptionWordInfo.module.scss";
+import {connect} from "react-redux";
 
-const DescriptionWordInfo = () => {
+const DescriptionWordInfo = (props) => {
   return (
     <>
-      <h1>Test title</h1>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
+      <h1>{props.searchWord}</h1>
+      <div>
+        <p>{props.definition}</p>
+        <p>{props.description}</p>
+      </div>
     </>
   );
 };
 
-export default DescriptionWordInfo;
+const mapStateToProps = (state) => {
+  return {
+    searchWord: state.general.searchWord,
+    description: state.summary.summary,
+    definition: state.dictionary.definition,
+  };
+};
+
+export default connect(mapStateToProps)(DescriptionWordInfo);
