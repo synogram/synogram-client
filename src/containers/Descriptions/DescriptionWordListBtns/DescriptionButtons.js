7f153d36b9154381bf11fa598d7e7ddda7d504c2
jhwan7 <46652246+jhwan7@users.noreply.github.com>
2021-01-19 03:31:55 -0500

    add action to related word list in description modal

--- src/containers/Descriptions/DescriptionWordListBtns/DescriptionButtons.js
+++ src/containers/Descriptions/DescriptionWordListBtns/DescriptionButtons.js
@@ -1,24 +1,32 @@
 import React from "react";
 import Aux from "../../../hoc/auxilary.js";
 import styles from "./DescriptionWordListBtns.module.scss";
+import {connect} from "react-redux";
+import * as actionTypes from "../../../store/index";
 
 const DescriptionWordListBtns = (props) => {
   const randomWords = props.wordListArr.map((word, index) => {
     let style = {};
     if (index < 3) {
-      style = { backgroundColor: "#FD5D08" };
+      style = {backgroundColor: "#FD5D08"};
     } else if (index < 6) {
-      style = { backgroundColor: "#FE9B08" };
+      style = {backgroundColor: "#FE9B08"};
     } else if (index < 9) {
-      style = { backgroundColor: "#FFD908" };
+      style = {backgroundColor: "#FFD908"};
     } else {
-      style = { backgroundColor: "#FFFCEB" };
+      style = {backgroundColor: "#FFFCEB"};
     }
     return (
       <button
         key={word + index}
         className={styles.wordListButtons}
         style={style}
+        onClick={() => {
+          props.storeSearchWord(word);
+          props.addRelatedWord(word);
+          props.getSummary(word);
+          props.getWordDictionary(word);
+        }}
       >
         {word}
       </button>
@@ -27,4 +35,15 @@ const DescriptionWordListBtns = (props) => {
   return <Aux>{randomWords}</Aux>;
 };
 
-export default DescriptionWordListBtns;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addRelatedWord: (value) => dispatch(actionTypes.addRelatedWords(value)),
+    getSummary: (value) => dispatch(actionTypes.getSummary(value)),
+    getWordDictionary: (value) =>
+      dispatch(actionTypes.getWordDictionary(value)),
+    storeSearchWord: (value) =>
+      dispatch(actionTypes.storeSearchWord({searchWord: value})),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(DescriptionWordListBtns);
