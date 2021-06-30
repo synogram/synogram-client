efd261c77737f572f2659fa3de0bb7974ca305dc
jhwan7 <46652246+jhwan7@users.noreply.github.com>
2021-06-30 18:45:56 -0400

    center description on landing page

--- src/containers/Pages/HomePage/LandingPage.js
+++ src/containers/Pages/HomePage/LandingPage.js
@@ -8,50 +8,16 @@ import {RESET_REDUX_STATE} from "../../../store/actions/actionTypes";
 import Logo from "../../../components/Logo/Logo";
 
 class LandingPage extends React.Component {
-  state = {
-    scrollbarVisible: false,
-  };
-
   magnifyGlassId = "Magnifying_Glass";
 
   componentDidMount() {
-    if (
-      this.getElementById(this.magnifyGlassId).clientHeight >document.body.clientHeight || document.body.scrollHeight > document.body.clientHeight
-    ) {
-      this.setState({scrollbarVisible: true});
-    }
-
     // If you return to the landingPage. The redux state has to reset.
     this.props.resetRedux();
   }
-  getElementById = (id) => {
-    try {
-      return document.getElementById(id);
-    } catch (error) {
-      console.log("Element not found: " + error);
-    }
-  };
 
   render() {
-    // takes into account background not covering absolute positioned item
-    const extendBackground = this.state.scrollbarVisible
-      ? {
-          minHeight:
-            Math.max(
-              this.getElementById(this.magnifyGlassId).clientHeight,
-              document.body.scrollHeight
-            ) + "px",
-        }
-      : {};
-
     return (
-      <div className={styles.landingPage} style={extendBackground}>
-        <div id={this.magnifyGlassId} className={styles.magnifyGlass}>
-          <div className={styles.outerGlass} />
-          <div className={styles.innerGlass} />
-          <div className={styles.glassHandle} />
-        </div>
-
+      <div className={styles.landingPage}>
         <div className={styles.content}>
           <NavBar className={styles.landingPageNavbar} />
           <Logo />
@@ -59,6 +25,11 @@ class LandingPage extends React.Component {
           <div className={styles.landingPageDescription}>
             <p>{messages.landingPageDescription}</p>
           </div>
+        </div>
+        <div id={this.magnifyGlassId} className={styles.magnifyGlass}>
+          <div className={styles.outerGlass} />
+          <div className={styles.innerGlass} />
+          <div className={styles.glassHandle} />
         </div>
       </div>
     );
