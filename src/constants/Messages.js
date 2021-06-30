bf690db82b0c888d37bdff1170d3beb0e80df9a0
jhwan7 <46652246+jhwan7@users.noreply.github.com>
2021-06-30 18:22:29 -0400

    Update Messages.js

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -8,15 +8,15 @@ const messages = {
 
   //HOMEPAGE
   landingPageDescription:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    "Welcome to Synogram! Synogram is an innovative search engine that visualizes web information using a diagram. Enter some keywords to beginning your journey",
 
   //ABOUTPAGE
   about_page_title: "Goal",
-  about_page_subtitle: "Hi we are trying.",
+  about_page_subtitle: "Welcome to Synogram!",
   about_page_description1:
-    'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
+    "The current mechanics of search and browsing information from the web is sequential. Users of search engines have to read over massive amounts of text sequentially to find the best result.",
   about_page_description2:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    " Our goal is to create a website that summarizes massive amounts of information from the web and provides a visual and interactive way of displaying results. Thus users have a higher dimension way of browsing information and learning about new topics from the web.",
 };
 
 export default messages;
