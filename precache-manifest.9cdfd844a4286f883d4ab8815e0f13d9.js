c195678ce5c83c41fedaee03e7182d9679c22ee3
jhwan7 <46652246+jhwan7@users.noreply.github.com>
2021-07-17 01:34:54 -0400

    Updates

---
+++ precache-manifest.9cdfd844a4286f883d4ab8815e0f13d9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "891214fd8daff777e605577b3a0334ad",
+    "url": "/synogram-client/index.html"
+  },
+  {
+    "revision": "22c3b4fe26d9519307b6",
+    "url": "/synogram-client/static/css/main.d6d686fd.chunk.css"
+  },
+  {
+    "revision": "eabcbb7e56fc26a7bb56",
+    "url": "/synogram-client/static/js/2.9b75ebf3.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/synogram-client/static/js/2.9b75ebf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22c3b4fe26d9519307b6",
+    "url": "/synogram-client/static/js/main.76a81718.chunk.js"
+  },
+  {
+    "revision": "5b2815ba1ba6bebffabf",
+    "url": "/synogram-client/static/js/runtime-main.ecc7bcf1.js"
+  },
+  {
+    "revision": "77757a49334f1c5a0d5db998b5bc5d7c",
+    "url": "/synogram-client/static/media/descriptionModalUpChevron.77757a49.svg"
+  },
+  {
+    "revision": "a132ad6ec4305cd336242894ca0cc7aa",
+    "url": "/synogram-client/static/media/email-logo.a132ad6e.svg"
+  },
+  {
+    "revision": "5f41266bdfeaeda3641d4ded4f606e7e",
+    "url": "/synogram-client/static/media/erin.5f41266b.jpg"
+  },
+  {
+    "revision": "e213a556f53bf722f84550a1a1f2023a",
+    "url": "/synogram-client/static/media/github-logo.e213a556.svg"
+  },
+  {
+    "revision": "7ae8ab2a27ffe9338094647c0ae8615e",
+    "url": "/synogram-client/static/media/jeongwon.7ae8ab2a.jpg"
+  },
+  {
+    "revision": "9a92235ee1306cf99c45bd01a51d0126",
+    "url": "/synogram-client/static/media/junseo.9a92235e.jpg"
+  },
+  {
+    "revision": "bef9f5ecc3097ab28383d91a4dd09822",
+    "url": "/synogram-client/static/media/linkedin-logo.bef9f5ec.svg"
+  },
+  {
+    "revision": "66f589f7852dd9b98790715c23f991ec",
+    "url": "/synogram-client/static/media/matt.66f589f7.jpg"
+  },
+  {
+    "revision": "18e144d0711725f0b210f417cc3d1e9d",
+    "url": "/synogram-client/static/media/synogramIcon.18e144d0.svg"
+  }
+]);
